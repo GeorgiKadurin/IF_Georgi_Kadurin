4bd5e43c75185fc833655595368f7f9ba895cd8f
opilane <opilane@HP-CZC6297B9X>
2022-12-13 10:07:31 +0200

    Add project files.

---
+++ IF_Georgi_Kadurin.py
@@ -0,0 +1,58 @@
+﻿
+from math import *
+from random import *
+print("Распорядок дня")
+
+try: kell=int(input("Mis kell on praegu?  "))
+except :
+    print("!!!!!!")
+if kell==(0):
+    print(f"magan")
+elif kell==(1):
+    print("veel magan")
+elif kell==(2):
+    print("tõusis vett jooma")
+elif kell==(3):
+    print("magan")
+elif kell==(4):
+    print("veel magan")
+elif kell==(5):
+    print("tõusis tualetti")
+elif kell==(6): 
+    print("ärkas üles")
+elif kell==(7):
+    print("lähen koolis")
+elif kell==(8):
+    print("koolis")
+elif kell==(9):
+    print("ma õpin")
+elif kell==(10):
+    print("veel õpin")
+elif kell==(11):
+    print("sõin kohvikus")
+elif kell==(12):
+    print("jäi haigeks")
+elif kell==(13): 
+    print("viidi haiglasse")
+elif kell==(14):
+    print("andis tablette")  
+elif kell==(15):
+    print("magan jääma")
+elif kell==(16):
+    print("magan")
+elif kell==(17):
+    print("magan")
+elif kell==(18):
+    print("tuli arst")
+elif kell==(19):
+    print("haiglast välja lastud")
+elif kell==(20):
+    print("kodus")
+elif kell==(21): 
+    print("sööma")
+elif kell==(22):
+    print("tööd tegemas")   
+elif kell==(23): 
+    print("Arvuti mängima")
+#-------------------------------------
+   
